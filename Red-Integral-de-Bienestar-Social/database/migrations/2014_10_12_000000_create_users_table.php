9803e42d263e0e115f6ccda738901ab43d29ba43
Charlinio <charlytorresg@gmail.com>
2018-02-27 17:39:29 -0700

    Tabla users

--- Red-Integral-de-Bienestar-Social/database/migrations/2014_10_12_000000_create_users_table.php
+++ Red-Integral-de-Bienestar-Social/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('privilegios');
+            $table->string('img_perfil');
             $table->rememberToken();
             $table->timestamps();
         });
